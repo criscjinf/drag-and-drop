6b14a66c73a4811846c9e92c923c05c7f40a1973
cristianolemos-somma <cristiano.lemos@somma-it.com>
2023-07-06 16:02:59 -0300

    Removido token

--- src/components/enviarImagem.js
+++ src/components/enviarImagem.js
@@ -9,8 +9,7 @@ export const enviarImagem = async (imagem, name, type) => {
   formData.append('contentType', `application/${type}`);
   const config = {
     headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXF1ZXN0ZXIiOiJjcmlzdGlhbm8ubGVtb3NAc29tbWEtaXQuY29tIiwiZXhwIjoxNjg4NzEyMDg3LCJzdWIiOiI4ZDZmOWNhOS04NjY4LTQ0MmEtOTQ1ZC1kZWQ3NWFkMzE5NTgifQ.b1yXXfQydLI-40uhL4IHfAsBKmJoVhb2_vC_59kV6J0',
+      Authorization: 'Bearer YOUR_TOKEN',
       'Content-Type': 'multipart/form-data',
     },
   };
